327701601b570cc403aae7b420fb21f1256d80cc
fatihbasar <fbasar680@gmail.com>
2022-03-10 03:20:32 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ifElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi % 2 == 1){
+            Console.WriteLine("Weird");
+            }else if(sayi % 2 == 0 && 2 <= sayi  && sayi >= 5 ) {
+             Console.WriteLine("Not Weird");
+            }else if(sayi % 2 == 0 && 6 <= sayi  && sayi >= 20 ){
+            Console.WriteLine("Weird");
+             }else if(sayi % 2 == 0 &&  sayi > 20) {
+            Console.WriteLine("Not Weird");
+             }
+
+            int time = DateTime.Now.Hour;
+            string sonuc = time <= 18 ? "Tunaydin" : "Iyi aksamlar";
+            Console.WriteLine(sonuc);
+           
+        }
+
+      
+    }
+}
